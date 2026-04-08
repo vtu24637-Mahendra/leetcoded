6143bdfc79e5fe0538629d1cf29987b3442940e6
vtu24637-Mahendra <vtu24637@veltech.edu.in>
2026-04-08 09:42:07 +0530

    Time: 1 ms (100.00%) | Memory: 45.7 MB (29.45%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        dfs(root, targetSum, path, res);
+        return res;
+    }
+
+    void dfs(TreeNode node, int sum, List<Integer> path, List<List<Integer>> res) {
+        if(node == null) return;
+
+        path.add(node.val);
+
+        if(node.left == null && node.right == null && sum == node.val) {
+            res.add(new ArrayList<>(path));
+        } else {
+            dfs(node.left, sum - node.val, path, res);
+            dfs(node.right, sum - node.val, path, res);
+        }
+
+        path.remove(path.size() - 1);
+    }
+}
