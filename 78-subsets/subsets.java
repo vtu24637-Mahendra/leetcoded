90c25a5daf16661cafaca941e08a4ee960533e9e
vtu24637-Mahendra <vtu24637@veltech.edu.in>
2026-05-01 17:22:22 +0530

    Time: 1 ms (88.86%) | Memory: 44.3 MB (39.17%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    void backtrack(int[] nums, int index, List<Integer> temp, List<List<Integer>> res) {
+        res.add(new ArrayList<>(temp));
+
+        for (int i = index; i < nums.length; i++) {
+            temp.add(nums[i]);
+            backtrack(nums, i + 1, temp, res);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
